7569a1a2c1240f5c21ceb8a58904965cb8b633c3
Anton Egorov <aegorov@aligntech.com>
2024-07-05 09:49:48 +0300

    Updated script

--- tg_api.js
+++ tg_api.js
@@ -1,47 +1,55 @@
 const axios = require('axios');
-const { Driver, getLogger, Ydb } = require('ydb-sdk');
+const { Driver, Ydb } = require('ydb-sdk');
 
-async function sendMessage(telegramToken, chatId, text) {
+async function fetchBdaysAndSendMessage(telegramToken, ydbEndpoint, ydbDatabase, ydbTable, chatId) {
   const TELEGRAM_URL = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
-  try {
-    const response = await axios.post(TELEGRAM_URL, {
-      chat_id: chatId,
-      text: text,
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error sending message:', error.response ? error.response.data : error.message);
-    throw error;
-  }
-}
 
-async function getBdaysFromYDB(endpoint, database, tableName) {
-  const authService = new Ydb.Auth({
-    anonymous: true,
-  });
-
-  const driver = new Driver({
-    endpoint: endpoint,
-    database: database,
-    authService,
-  });
-
-  await driver.ready(10000); // Подключение к базе данных с таймаутом 10 секунд
-
-  const session = await driver.tableClient.session();
-  const query = `SELECT * FROM ${tableName}`;
-  const { resultSets } = await session.executeQuery(query);
-
-  const rows = [];
-  for (const resultSet of resultSets) {
-    for (const row of resultSet.rows) {
-      rows.push(row.items.map(item => item.textValue));
+  async function sendMessage(chatId, text) {
+    try {
+      const response = await axios.post(TELEGRAM_URL, {
+        chat_id: chatId,
+        text: text,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error sending message:', error.response ? error.response.data : error.message);
+      throw error;
     }
   }
 
-  await session.release();
+  async function getBdaysFromYDB() {
+    const authService = new Ydb.Auth({
+      anonymous: true,
+    });
 
-  return rows;
+    const driver = new Driver({
+      endpoint: ydbEndpoint,
+      database: ydbDatabase,
+      authService,
+    });
+
+    await driver.ready(10000);
+
+    const session = await driver.tableClient.session();
+    const query = `SELECT * FROM ${ydbTable}`;
+    const { resultSets } = await session.executeQuery(query);
+
+    const rows = [];
+    for (const resultSet of resultSets) {
+      for (const row of resultSet.rows) {
+        rows.push(row.items.map(item => item.textValue));
+      }
+    }
+
+    await session.release();
+    return rows;
+  }
+
+  const bdays = await getBdaysFromYDB();
+  let responseMessage = 'Данные из таблицы bdays_tbl:\n';
+  responseMessage += bdays.map(row => row.join(', ')).join('\n');
+
+  return await sendMessage(chatId, responseMessage);
 }
 
-module.exports = { sendMessage, getBdaysFromYDB };
+module.exports = { fetchBdaysAndSendMessage };
